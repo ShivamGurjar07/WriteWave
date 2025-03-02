7c862018e0b5b23662dfd0e9d867c9824fb5f7ff
ShivamGurjar07 <gurjarcvam07@gmail.com>
2025-03-02 19:50:54 +0530

    add files

--- Backend/index.js
+++ Backend/index.js
@@ -19,6 +19,8 @@ const PORT = process.env.PORT;
 const secret = process.env.JWT_SECRET || shivam;
 
 app.use(cors({origin: "http://localhost:5173", credentials: true, }));
+// app.use(cors());
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
